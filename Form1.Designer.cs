2ea42e5ebed2220b87867cf00183d0a5bef7b45b
bomb-inside <bomb@DESKTOP-L8F8Q2D.localdomain>
2020-09-29 16:56:22 +0900

    MDI practice

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asdfToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem,
+            this.NewToolStripMenuItem,
+            this.asdfToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1171, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.FileToolStripMenuItem.Text = "&File";
+            // 
+            // asdfToolStripMenuItem
+            // 
+            this.asdfToolStripMenuItem.Name = "asdfToolStripMenuItem";
+            this.asdfToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.asdfToolStripMenuItem.Text = "&Close";
+            // 
+            // NewToolStripMenuItem
+            // 
+            this.NewToolStripMenuItem.Name = "NewToolStripMenuItem";
+            this.NewToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.NewToolStripMenuItem.Text = "&New";
+            this.NewToolStripMenuItem.Click += new System.EventHandler(this.NewToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(950, 494);
+            this.ClientSize = new System.Drawing.Size(1171, 653);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asdfToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem NewToolStripMenuItem;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,47 @@ namespace Practice
         {
             InitializeComponent();
         }
+
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 newMDIChild = new Form2();
+            // Set the Parent Form of the Child window.
+            newMDIChild.MdiParent = this;
+            // Display the new form.
+            newMDIChild.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            /*
+            --Button Example--
+            public Button button1;
+            public Form1()
+            {
+                button1 = new Button();
+                button1.Size = new Size(100, 100);
+                button1.Location = new Point(50, 50);
+                button1.Text = "Click me";
+                this.Controls.Add(button1);
+                button1.Click += new EventHandler(button1_Click);
+            }
+            private void button1_Click(object sender, EventArgs e)
+            {
+                MessageBox.Show("Hello World");
+            }
+            */
+
+            //protected void MDIChildNew_Click(object sender, System.EventArgs e)
+            //{
+            //Form2 newMDIChild = new Form2();
+            // Set the Parent Form of the Child window.
+            //newMDIChild.MdiParent = this;
+            // Display the new form.
+            //newMDIChild.Show();
+            //}
+        }
+
     }
 }
